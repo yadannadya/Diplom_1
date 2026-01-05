695764098da94af42feeeb1a090cd6a581200480
Nadya <nadezhdapodlesnaia@yandex.ru>
2026-01-05 22:15:10 +0300

    First commit

---
+++ data.py
@@ -0,0 +1,7 @@
+from praktikum.ingredient_types import INGREDIENT_TYPE_SAUCE, INGREDIENT_TYPE_FILLING
+
+
+
+bun = (["red", 100], ["black", 150])
+sauce = ([INGREDIENT_TYPE_SAUCE, "pink", 50], [INGREDIENT_TYPE_SAUCE, "blue", 150] )
+filling = ([INGREDIENT_TYPE_FILLING, "green", 150], [INGREDIENT_TYPE_FILLING, "color", 200])
